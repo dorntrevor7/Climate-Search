4b5e755d7be87d5b4899941c0ad590f268376c9f
dorntrevor7 <dorntrevor7@gmail.com>
2022-09-26 22:53:14 -0700

    api working with consoled main

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Climate-Search
+
 This is an app using a weather api that returns back the city you search for!
+
+## Description
+
+This is a quiz to test your coding knowledge with different questions and the ability to save your score even if the page reloads.
+
+## Image
+
+![This is an image of the landing page of The Climate Search](./assets/images/climateSearch.png)
+
+## Usage
+
+This is used to test your abilities to code without any "google fooing" you can type in your score with your name after you are done playing!
+
+This site was built using Github Pages, check it out: [Climate Search!](https://dorntrevor7.github.io/Climate-Search/).
---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+const $cityInput = $("#cityInput");
+
+const currentApi = `api.openweathermap.org/data/2.5/weather?q=${$cityInput}&APPID=09335e290f36cab7997b39556d93e753`;
+
+const forecastApi = `api.openweathermap.org/data/2.5/forecast?q=${$cityInput}&APPID=09335e290f36cab7997b39556d93e753`;
+
+fetch(
+  "https://api.openweathermap.org/data/2.5/weather?q=tempe&appid=09335e290f36cab7997b39556d93e753",
+  {
+    method: "GET", //GET is the default.
+  }
+)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    const name = data.name;
+    const temp = (data.main.temp - 273.15) * 1.8 + 32;
+    const wind = data.wind.speed;
+    const humid = data.main.humidity;
+    console.log(name, temp.toFixed(2), wind, humid);
+  });
