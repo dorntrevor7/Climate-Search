f55f7b58641db33a168b1d9dbcdb57f3e4a3e60d
dorntrevor7 <dorntrevor7@gmail.com>
2022-09-27 21:34:08 -0700

    readme and js tweaks

--- README.md
+++ README.md
@@ -4,14 +4,14 @@ This is an app using a weather api that returns back the city you search for!
 
 ## Description
 
-This is a quiz to test your coding knowledge with different questions and the ability to save your score even if the page reloads.
+This is an app that uses the open weather API to search a city for their weather as well as the 5 day forecast
 
 ## Image
 
-![This is an image of the landing page of The Climate Search](./assets/images/climateSearch.png)
+![This is an image of the landing page of The Climate Search](./assets/images/weatherD.png)
 
 ## Usage
 
-This is used to test your abilities to code without any "google fooing" you can type in your score with your name after you are done playing!
+This is used for vacations or work trips. This will allow you to know how to dress for your trip and what the weather will look and be like.
 
 This site was built using Github Pages, check it out: [Climate Search!](https://dorntrevor7.github.io/Climate-Search/).
--- assets/js/script.js
+++ assets/js/script.js
@@ -7,30 +7,39 @@ const $temp = document.getElementById("temp");
 const $wind = document.getElementById("wind");
 const $humid = document.getElementById("humidity");
 const $savedItems = document.getElementById("savedItems");
+const forecastRow = document.getElementById("forecastRow");
 const storage = [];
 
-function apiCall() {
-  const searchInput = document.getElementById("searchInput").value;
+async function apiCall() {
+  let searchInput = document.getElementById("searchInput").value;
   storage.push(searchInput);
 
-  fetch(
+  await fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${searchInput}&appid=09335e290f36cab7997b39556d93e753`,
     {
       method: "GET", //GET is the default.
     }
   )
     .then(function (response) {
+      searchInput = "";
       return response.json();
     })
     .then(function (data) {
       $searchCity.innerHTML = data.name;
-      $temp.innerHTML = ((data.main.temp - 273.15) * 1.8 + 32).toFixed(2);
+      $temp.innerHTML = ((data.main.temp - 273.15) * 1.8 + 32).toFixed(0);
       $wind.innerHTML = data.wind.speed;
       $humid.innerHTML = data.main.humidity;
+      const $li = document.createElement("li");
+      $li.textContent = data.name.toUpperCase();
+      $li.setAttribute("class", "btn btn-outline-success");
+      $li.setAttribute("style", "margin-bottom: 10px; letter-spacing: .2rem;");
+      $li.setAttribute("type", "button");
+      $li.setAttribute("value", data.name);
+      $savedItems.append($li);
       localStorage.setItem("location", JSON.stringify(storage));
     });
 
-  fetch(
+  await fetch(
     "https://api.openweathermap.org/data/2.5/forecast?q=tempe&cnt=5&appid=09335e290f36cab7997b39556d93e753",
     {
       method: "GET",
@@ -40,7 +49,21 @@ function apiCall() {
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
+      data.list.forEach((day) => {
+        let section = document.createElement("section");
+        section.setAttribute("class", "col forecastDay");
+        let h5 = document.createElement("h5");
+        let temp = document.createElement("h6");
+        let wind = document.createElement("h6");
+        let humid = document.createElement("h6");
+        h5.innerHTML = day.dt;
+        temp.innerHTML =
+          "Temp: " + ((day.main.temp - 273.15) * 1.8 + 32).toFixed(0);
+        wind.innerHTML = "Wind: " + day.wind.speed;
+        humid.innerHTML = "Humid: " + day.main.humidity;
+        section.append(h5, temp, wind, humid);
+        forecastRow.append(section);
+      });
     });
 }
 
@@ -56,39 +79,67 @@ saved.forEach((location) => {
   $li.setAttribute("style", "margin-bottom: 10px; letter-spacing: .2rem;");
   $li.setAttribute("type", "button");
   $li.setAttribute("value", location);
+  localStorage.setItem("location", JSON.stringify(storage));
   $savedItems.append($li);
 });
 
-const $liSave = document.querySelector("li");
-$liSave.addEventListener("click", () => {
-  const buttonVal = $liSave.getAttribute("value");
-  console.log(buttonVal);
-  fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${buttonVal}&appid=09335e290f36cab7997b39556d93e753`,
-    {
-      method: "GET", //GET is the default.
-    }
-  )
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      $searchCity.innerHTML = data.name;
-      $temp.innerHTML = ((data.main.temp - 273.15) * 1.8 + 32).toFixed(2);
-      $wind.innerHTML = data.wind.speed;
-      $humid.innerHTML = data.main.humidity;
-    });
+const $liSave = document.querySelectorAll("li");
 
-  fetch(
-    "https://api.openweathermap.org/data/2.5/forecast?q=tempe&cnt=5&appid=09335e290f36cab7997b39556d93e753",
-    {
-      method: "GET",
+$liSave.forEach((li) => {
+  li.addEventListener(
+    "click",
+
+    async function runbutton() {
+      const buttonVal = li.getAttribute("value");
+      console.log(buttonVal);
+      await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${buttonVal}&appid=09335e290f36cab7997b39556d93e753`,
+        {
+          method: "GET", //GET is the default.
+        }
+      )
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          $searchCity.innerHTML = data.name;
+          $temp.innerHTML = ((data.main.temp - 273.15) * 1.8 + 32).toFixed(0);
+          $wind.innerHTML = data.wind.speed;
+          $humid.innerHTML = data.main.humidity;
+        });
+
+      await fetch(
+        `https://api.openweathermap.org/data/2.5/forecast?q=${buttonVal}&cnt=5&appid=09335e290f36cab7997b39556d93e753`,
+        {
+          method: "GET",
+        }
+      )
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          data.list.forEach((day) => {
+            let section = document.createElement("section");
+            section.setAttribute("class", "col forecastDay");
+            let h5 = document.createElement("h5");
+            let temp = document.createElement("h6");
+            let wind = document.createElement("h6");
+            let humid = document.createElement("h6");
+            h5.innerHTML = day.dt;
+            temp.innerHTML =
+              "Temp: " + ((day.main.temp - 273.15) * 1.8 + 32).toFixed(0);
+            wind.innerHTML = "Wind: " + day.wind.speed;
+            humid.innerHTML = "Humid: " + day.main.humidity;
+            section.append(h5, temp, wind, humid);
+            forecastRow.append(section);
+          });
+        });
+      //     <section class="col forecastDay">
+      //     <h5 id="date">date: </h5>
+      //     <h6 id="temp">Temp: </h6>
+      //     <h6 id="wind">Wind: </h6>
+      //     <h6 id="humidity">Humidity: </h6>
+      //     </section>
     }
-  )
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-    });
+  );
 });
